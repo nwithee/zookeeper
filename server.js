60fec377ba0b28d9d465652337ea94d49a0d8957
nzwithee@gmail.com <Cartoon#01>
2022-04-13 18:23:59 -0500

    changes

--- server.js
+++ server.js
@@ -33,6 +33,11 @@ function filterByQuery(query, animalsArray) {
     return filterResults;
 }
 
+function findByID(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 
 app.get('/api/animals', (req, res)=>{
     let results = animals;
@@ -44,8 +49,18 @@ app.get('/api/animals', (req, res)=>{
     res.json(results);
 });
 
+app.get('/api/animals/:id', (req, res) => {
+    const result = findByID(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } 
+    else {
+        res.send(404);
+    }   
+});
+
 app.listen(PORT, () => {
-    console.log('API server now on port ${PORT}!');
+    console.log(`API server now on port ${PORT}!`);
 });
 
 
